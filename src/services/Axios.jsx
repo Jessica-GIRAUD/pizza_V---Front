b1ea8ac38a7ad21dc4d69334c6a1959e634f2c6c
Jessica-GIRAUD <jessica.giraud@strateg.in>
2022-04-23 14:29:52 +0200

    cors

--- src/services/Axios.jsx
+++ src/services/Axios.jsx
@@ -6,6 +6,10 @@ export default axios.create({ baseURL: BASE_URL });
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
-  headers: { "Content-Type": "application/json" },
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+  },
   withCredentials: true,
 });
