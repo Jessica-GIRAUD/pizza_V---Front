7c29e221b25a0a4a670f1b1bb6ab6bb1cbad732b
Jessica-GIRAUD <jessica.giraud@strateg.in>
2022-04-23 15:52:03 +0200

    headers

--- src/services/Axios.jsx
+++ src/services/Axios.jsx
@@ -2,22 +2,10 @@ import axios from "axios";
 
 const BASE_URL = "https://pizza-kika.herokuapp.com";
 
-export default axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Origin": "https://pizza-kika.netlify.app",
-  },
-  withCredentials: true,
-});
+export default axios.create({ baseURL: BASE_URL });
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Origin": "https://pizza-kika.netlify.app",
-  },
+  headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
