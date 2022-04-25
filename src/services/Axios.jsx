f455665040f581523e2fa9fcdae78bbe3d865835
Jessica-GIRAUD <jessica.giraud@strateg.in>
2022-04-25 12:52:10 +0200

    test cors

--- src/services/Axios.jsx
+++ src/services/Axios.jsx
@@ -2,7 +2,11 @@ import axios from "axios";
 
 const BASE_URL = "https://pizza-kika.herokuapp.com";
 
-export default axios.create({ baseURL: BASE_URL, withCredentials: true });
+export default axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
