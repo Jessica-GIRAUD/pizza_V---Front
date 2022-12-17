223c721661038e08be46cf067bdffc1456daac8d
Jessica-GIRAUD <jessica.giraud@strateg.in>
2022-12-17 12:43:49 +0100

    change back url

--- src/services/Routes.jsx
+++ src/services/Routes.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://pizza-kika.herokuapp.com";
+const BASE_URL = "https://pizza-kika.onrender.com";
 // const BASE_URL = "http://localhost:5001";
 
 export const getAllPublic = async (topic) => {
